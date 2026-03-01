ca6cff4777c954b3f09fd96d24c1e68f7f865a0c
vxd <ved2997@yahoo.com>
2026-03-01 18:33:19 +0000

    improved framesplit code

--- frameSplit.py
+++ frameSplit.py
@@ -1,46 +1,30 @@
 import cv2
 
-def convertFrame(frame): # convert frame to size 8x8
-    return cv2.resize(frame, (8, 8))
-
-def readFrames(): # read frames and put in file
+def getFrames(path: str):
+    video = cv2.VideoCapture(path)
+    if not video.isOpened():
+        raise FileNotFoundError()
     
-    video = cv2.VideoCapture("bad apple.mp4")
+    try:
+        while True:
+            success, frame = video.read()
+            if not success:
+                break
+            yield frame
+    finally:
+        video.release()
 
-    frameCount = 1
-    success = True
-    #fps = video.get(cv2.CAP_PROP_FPS)
-    #delay = int(1000 / fps)
-    while success:
-        success, frame = video.read()
-        #cv2.namedWindow("video")
-        if success:
-            """
-            cv2.imshow("video", frame)
-            cv2.waitKey(delay)
-            cv2.destroyWindow(f"frame{frameCount}")
-            """
-            cv2.imwrite(f"frames/frame{frameCount}.png", frame)
-            frameCount+=1
 
-    video.release()
-    #cv2.destroyAllWindows()
-    return
+def convertFrames(frames):
+    for frame in frames:
+        yield cv2.resize(frame, (8, 8))
 
-def showFrames():
-    video = cv2.VideoCapture("bad apple.mp4")
+def showFrames(frames):
+    video = cv2.namedWindow("video")
+    for frame in frames:
+        cv2.imshow("video", frame)
+        cv2.waitKey(16) # bcos 1000/60fps
 
-    frameCount = 1
-    success = True
-    fps = video.get(cv2.CAP_PROP_FPS)
-    delay = int(1000 / fps)
-    cv2.namedWindow("video")
-    while success:
-        success, frame = video.read()
-        if success:
-            cv2.imshow("video", frame)
-            cv2.waitKey(delay)
-
-            frameCount+=1
-    cv2.destroyAllWindows()
-readFrames()
+def downloadFrames(frames):
+    for frameCount, frame in enumerate(frames, start=1):
+        cv2.imwrite(f"frames/frame{frameCount}.png", frame)
